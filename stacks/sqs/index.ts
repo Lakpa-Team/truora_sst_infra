754e77bc637193dcd1162b70963cdeb9e9065e26
RodLopezDev <rlopez@lakpa.cl>
2023-11-30 17:03:57 -0500

    fix: :bug: added new queue names

--- stacks/sqs/index.ts
+++ stacks/sqs/index.ts
@@ -1,12 +1,9 @@
 import { Queue, Stack } from "sst/constructs";
 
 export function GenerateSQS(stack: Stack) {
-  const mainQueue = new Queue(stack, "truora-integration-main");
-  const dlqQueue = new Queue(stack, "truora-integration-dlq");
-  const downloadQueue = new Queue(stack, "truora-integration-downloader");
-  const downloadDlqQueue = new Queue(
-    stack,
-    "truora-integration-downloader-dlq"
-  );
+  const mainQueue = new Queue(stack, "main-processor");
+  const dlqQueue = new Queue(stack, "main-dlq");
+  const downloadQueue = new Queue(stack, "downloader");
+  const downloadDlqQueue = new Queue(stack, "downloader-dlq");
   return { mainQueue, dlqQueue, downloadQueue, downloadDlqQueue };
 }
